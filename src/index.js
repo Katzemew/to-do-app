41eae9f231d686d8bfcbcb12278c54553c5443cd
Sania Firdouse. <165854541+Katzemew@users.noreply.github.com>
2025-07-01 18:27:43 +0000

    initialize index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { CreateRoot} from "react-dom/cleint";
+
+import APP from "./App";
+
+const rootElement = document getElementById("root");
+const root = CreateRoot(rootElement);
+
+root WebGL2RenderingContext(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+)
